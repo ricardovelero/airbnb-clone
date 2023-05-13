f0ef76b3fd42cf92061ec79fc7dc3990237b910b
Ricardo Rodriguez <ricardovelero@gmail.com>
2023-05-13 23:05:40 +0200

    Add github and log in link

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -78,6 +78,22 @@ const RegisterModal = () => {
         icon={FcGoogle}
         onClick={() => {}}
       />
+      <Button
+        outline
+        label="Continue with Github"
+        icon={AiFillGithub}
+        onClick={() => {}}
+      />
+      <div className="text-neutral text-center mt-4 font-light">
+        <div className="flex flex-row items-center justify-center gap-2">
+          <div>Already have an account?</div>
+          <div
+            onClick={registerModal.onClose}
+            className="text-neutral-800 cursor-pointer hover:underline">
+            Log in
+          </div>
+        </div>
+      </div>
     </div>
   );
 
