ace2ad3471708ce3ba243e20c988c920954445f6
Ricardo Rodriguez <ricardovelero@gmail.com>
2023-05-15 19:34:10 +0200

    Add avatar src

--- app/components/Avatar.tsx
+++ app/components/Avatar.tsx
@@ -2,12 +2,16 @@
 
 import Image from 'next/image';
 
-const Avatar = () => {
+interface AvatarProps {
+  src?: string | null | undefined;
+}
+
+const Avatar: React.FC<AvatarProps> = ({ src }) => {
   return (
     <div>
       <Image
         className="rounded-full"
-        src={'/images/placeholder.jpg'}
+        src={src || '/images/placeholder.jpg'}
         height={30}
         width={30}
         alt="Avatar"
