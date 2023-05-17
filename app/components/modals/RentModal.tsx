524f8d0710a72e1aba6aea8956533ca492dd4ae1
Ricardo Rodriguez <ricardovelero@gmail.com>
2023-05-17 12:11:34 +0200

    Add map to country select

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -8,6 +8,7 @@ import { categories } from '../navbar/Categories';
 import CategoryInput from '../Inputs/CategoryInput';
 import { FieldValues, useForm } from 'react-hook-form';
 import CountrySelect from '../Inputs/CountrySelect';
+import dynamic from 'next/dynamic';
 
 enum STEPS {
   CATEGORY = 0,
@@ -46,6 +47,11 @@ const RentModal = () => {
 
   const category = watch('category');
   const location = watch('location');
+
+  const Map = useMemo(
+    () => dynamic(() => import('../Map'), { ssr: false }),
+    [location]
+  );
 
   const setCustomValue = (id: string, value: any) => {
     setValue(id, value, {
@@ -106,6 +112,7 @@ const RentModal = () => {
           value={location}
           onChange={(value) => setCustomValue('location', value)}
         />
+        <Map center={location?.latlng} />
       </div>
     );
   }
