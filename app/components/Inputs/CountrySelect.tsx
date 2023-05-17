c4544d62805083aa66c56f602b87574534ef1633
Ricardo Rodriguez <ricardovelero@gmail.com>
2023-05-17 12:11:20 +0200

    Fix typo

--- app/components/Inputs/CountrySelect.tsx
+++ app/components/Inputs/CountrySelect.tsx
@@ -6,7 +6,7 @@ import Select from 'react-select';
 export type CountrySelectValue = {
   flag: string;
   label: string;
-  latlong: number[];
+  latlng: number[];
   region: string;
   value: string;
 };
