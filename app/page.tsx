d2aa7c760d484afb027e55d5d16f57ed7c32b27b
Ricardo Rodriguez <ricardovelero@gmail.com>
2023-05-18 10:35:40 +0200

    Add empty state

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,18 @@
-
+import Container from '@/app/components/Container';
+import EmptyState from './components/EmptyState';
 
 export default function Home() {
+  const isEmpty = true;
+
+  if (isEmpty) return <EmptyState showReset />;
+
   return (
-    <div className="text-rose-500 text-2xl">Hello Airbnb!</div>
-  )
+    <>
+      <Container>
+        <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
+          <div>My future Listings</div>
+        </div>
+      </Container>
+    </>
+  );
 }
