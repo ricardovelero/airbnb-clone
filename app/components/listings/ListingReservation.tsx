4ac8461fac6489c3e58264771d5c97d3bfdf2d22
Ricardo Rodriguez <ricardovelero@gmail.com>
2023-05-26 12:42:54 +0200

    Add calendar and reservations

---
+++ app/components/listings/ListingReservation.tsx
@@ -0,0 +1,50 @@
+'use client';
+
+import { Range } from 'react-date-range';
+import Calendar from '../Inputs/Calendar';
+import Button from '../Button';
+
+interface ListingReservationProps {
+  price: number;
+  totalPrice: number;
+  dateRange: Range;
+  disabled?: boolean;
+  disabledDates: Date[];
+  onChangeDate: (value: Range) => void;
+  onSubmit: () => void;
+}
+
+const ListingReservation: React.FC<ListingReservationProps> = ({
+  price,
+  totalPrice,
+  dateRange,
+  disabled,
+  disabledDates,
+  onChangeDate,
+  onSubmit,
+}) => {
+  return (
+    <div className="bg-white rounded-xl border-[1px] border-neutral-200 overflow-hidden">
+      <div className="flex flex-row items-center gap-1 p-4">
+        <div className="text-2xl font-semibold">$ {price}</div>
+        <div className="font-light text-neutral-600">night</div>
+      </div>
+      <hr />
+      <Calendar
+        value={dateRange}
+        disabledDates={disabledDates}
+        onChange={(value: any) => onChangeDate(value.selection)}
+      />
+      <hr />
+      <div className="p-4">
+        <Button label="Reserve" disabled={disabled} onClick={onSubmit} />
+      </div>
+      <div className="p-4 felx flex-row items-center justify-between font-semibold text-lg">
+        <div>Total</div>
+        <div>$ {totalPrice}</div>
+      </div>
+    </div>
+  );
+};
+
+export default ListingReservation;
