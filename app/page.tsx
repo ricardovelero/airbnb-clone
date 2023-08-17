418630193516d6f95f7b9b5fbc4b0311d7222ba7
Ricardo Rodriguez <ricardovelero@gmail.com>
2023-08-17 12:52:12 +0200

    Takeout ClientOnly

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,6 @@ import EmptyState from '@/app/components/EmptyState';
 
 import getListings, { IListingsParams } from '@/app/actions/getListings';
 import getCurrentUser from '@/app/actions/getCurrentUser';
-import ClientOnly from './components/ClientOnly';
 
 interface HomeProps {
   searchParams: IListingsParams;
@@ -17,18 +16,13 @@ const Home = async ({ searchParams }: HomeProps) => {
   const currentUser = await getCurrentUser();
 
   if (listings.length === 0) {
-    return (
-      <ClientOnly>
-        <EmptyState showReset />
-      </ClientOnly>
-    );
+    return <EmptyState showReset />;
   }
 
   return (
-    <ClientOnly>
-      <Container>
-        <div
-          className="
+    <Container>
+      <div
+        className="
             pt-24
             grid 
             grid-cols-1 
@@ -39,16 +33,15 @@ const Home = async ({ searchParams }: HomeProps) => {
             2xl:grid-cols-6
             gap-8
           ">
-          {listings.map((listing: any) => (
-            <ListingCard
-              currentUser={currentUser}
-              key={listing.id}
-              data={listing}
-            />
-          ))}
-        </div>
-      </Container>
-    </ClientOnly>
+        {listings.map((listing: any) => (
+          <ListingCard
+            currentUser={currentUser}
+            key={listing.id}
+            data={listing}
+          />
+        ))}
+      </div>
+    </Container>
   );
 };
 
